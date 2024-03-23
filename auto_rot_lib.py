d5433e9e010fe61176a1f2c2a23adb0e645c49b9
Basilísco <senstur.art@gmail.com>
2024-03-23 13:41:21 -0300

    adição das verificações de erro

--- auto_rot_lib.py
+++ auto_rot_lib.py
@@ -9,14 +9,18 @@ def get_home_dir():
 def get_log_path():
     return os.path.join(get_home_dir(), "logs/")
 
+
 def get_erros_log_path():
     return os.path.join(get_log_path(), "errors/")
+
 
 def get_backup_path():
     return os.path.join(get_home_dir(), "logs_backup/")
 
+
 def autoRotLinuxPy_repo():
     return os.path.join(get_home_dir(), "py_scripts/AutoRotLinuxPy")
+
 
 def catch_error(resultado, file_name):
     if resultado.returncode != 0:
@@ -25,7 +29,7 @@ def catch_error(resultado, file_name):
         error_log_path = get_erros_log_path()
         data_hora = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
-        erro = data_hora + "\nErro: " + str(resultado.stderr)
+        erro = "{} - Erro: {}".format(data_hora, resultado.stderr)
 
         with open("{}err-{}.txt".format(error_log_path, file_name), "w",
                   encoding="utf-8") as error_log:
--- teste_apt_update.py
+++ teste_apt_update.py
@@ -3,7 +3,7 @@ from auto_rot_lib import catch_error
 
 def upgrade_packages():
     # Comando para atualizar os pacotes usando apt upgrade
-    comando = ["sudo", "apt8", "upgrade", "-y"]
+    comando = ["sudo", "apt", "upgrade", "-y"]
 
     # Executar o comando usando subprocess
     resultado = subprocess.Popen(comando, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
@@ -13,6 +13,7 @@ def upgrade_packages():
     stdout, stderr = resultado.communicate(input=b"yes\n")
 
     catch_error(resultado, "apt_update_upgrade")
+
 
 # Chamar a função para atualizar os pacotes
 upgrade_packages()
