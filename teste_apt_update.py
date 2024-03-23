039cd9ed8d68aaafed9166389c1305ff17bd3c09
Basilísco <senstur.art@gmail.com>
2024-03-23 12:27:11 -0300

    teste apt_update

--- teste_apt_update.py
+++ teste_apt_update.py
@@ -1,8 +1,26 @@
 import subprocess
 
-def apt_update():
-    comando = ["sudo", "apt", "update"]
-    subprocess.run(comando)
+def upgrade_packages():
+    # Comando para atualizar os pacotes usando apt upgrade
+    comando = "sudo apt upgrade -y"
+
+    # Executar o comando usando subprocess
+    processo = subprocess.Popen(comando, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+
+    # Enviar "yes" para a entrada padrão (stdin)
+    stdout, stderr = processo.communicate(input=b"yes\n")
+
+    # Capturar a saída do processo
+    output = stdout.decode("utf-8")
+    error = stderr.decode("utf-8")
+
+    # Verificar se houve erros durante a execução do comando
+    if processo.returncode != 0:
+        print("Erro ao executar apt upgrade:", error)
+    else:
+        print("Pacotes atualizados com sucesso:", output)
 
 
-apt_update()
+# Chamar a função para atualizar os pacotes
+upgrade_packages()
