c7573ede1f64f088cc20d41d618e7c9da04df764
Basilísco <senstur.art@gmail.com>
2024-03-22 23:51:37 -0300

    finalizar git_pull_routine

--- auto_rot_lib.py
+++ auto_rot_lib.py
@@ -1,18 +1,19 @@
 import os
 
+
 def get_home_dir():
     return os.path.expanduser("~")
+
 
 def get_log_path():
     return os.path.join(get_home_dir(), "logs/")
 
+
 def get_backup_path():
     return os.path.join(get_home_dir(), "logs_backup/")
 
-def get_git_AutoRotLinuxPy_path():
-    return os.path.join(get_home_dir(), "py_scripts/AutoRotLinuxPy")
 
-def get_git_Ominifood_path():
-    return os.path.join(get_home_dir(), "web/Omnifood")
+def autoRotLinuxPy_repo():
+    return os.path.join(get_home_dir(), "py_scripts/AutoRotLinuxPy")
 
 
--- git_pull_rotina.py
+++ git_pull_rotina.py
@@ -2,17 +2,21 @@
 ## Automatiza o git pull dos diretorios designados               ##
 ## Os scripts podem ser atualizados pelo IDE com mais facilidade ##
 ###################################################################
-from auto_rot_lib import get_git_AutoRotLinuxPy_path, get_git_Ominifood_path
+from auto_rot_lib import autoRotLinuxPy_repo
 
 import os
 import subprocess
 
 def puxar_repos():
-    repositorios = [get_git_AutoRotLinuxPy_path(), get_git_Ominifood_path()]
+    repositorios = [autoRotLinuxPy_repo()]
 
     for repo in repositorios:
         #Entra no repositorio
         os.chdir(repo)
+
+        # ELIMINA QUALQUER MUDANÇA NO REPO LOCAL!!
+        # (LINUX - MANTER AS MUDANÇAS E O PUSH NO IDE !!!!)
+        subprocess.run(["git", "reset", "--hard"])
 
         #Executa o comando
         subprocess.run(["git", "pull"])
