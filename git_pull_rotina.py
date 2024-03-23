473cf8e9feb892989c0fae901f30c75ab1f776d3
Basilísco <senstur.art@gmail.com>
2024-03-22 23:26:37 -0300

    teste git_pull_routine

--- git_pull_rotina.py
+++ git_pull_rotina.py
@@ -8,11 +8,16 @@ import subprocess
 
 def puxar_repos():
     repositorios = [get_git_AutoRotLinuxPy_path()]
+
+    print(repositorios)
     for _ in repositorios:
+
         entrar = ["cd", _]
+        print(entrar)
         subprocess.run(entrar)
 
         comando = ["git pull"]
+        print(comando)
         subprocess.run(comando)
 
 
