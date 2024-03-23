05e9a8f2be337ab37e3d4d087e3bcec3584ac9bf
Basilísco <senstur.art@gmail.com>
2024-03-23 13:15:24 -0300

    teste erro_log

--- auto_rot_lib.py
+++ auto_rot_lib.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 
@@ -14,8 +15,18 @@ def get_erros_log_path():
 def get_backup_path():
     return os.path.join(get_home_dir(), "logs_backup/")
 
-
 def autoRotLinuxPy_repo():
     return os.path.join(get_home_dir(), "py_scripts/AutoRotLinuxPy")
 
+def catch_error(resultado, file_name):
+    if resultado.returncode != 0:
+        # Imprimir a saída de erro
+        print("Erro:", resultado.stderr)
+        error_log_path = get_erros_log_path()
+        data_hora = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
+        erro = "\n" + data_hora + "Erro: " + resultado.stderr
+
+        with open("{}err-{}.txt".format(error_log_path, file_name), "w",
+                  encoding="utf-8") as error_log:
+            error_log.write(erro)
--- teste_erro_log.py
+++ teste_erro_log.py
@@ -1,5 +1,6 @@
 import subprocess
-from auto_rot_lib import get_erros_log_path
+from auto_rot_lib import catch_error
+
 
 def run_command():
     # Comando a ser executado
@@ -8,22 +9,9 @@ def run_command():
     # Executar o comando usando subprocess.run
     resultado = subprocess.run(comando, capture_output=True, text=True)
 
-    # Verificar se ocorreu um erro
-    if resultado.returncode != 0:
-        # Imprimir a saída de erro
-        print("Erro:", resultado.stderr)
-        error_log_path = get_erros_log_path()
+    catch_error(resultado, "teste_erro_log.py")
 
-        erro = "Erro: " + resultado.stderr
-        file = "teste_erro_log.py"
-
-        with open("{}error-{}.txt".format(error_log_path, file), "w",
-                  encoding="utf-8") as error_log:
-            error_log.write(erro)
-
-    else:
-        # Imprimir a saída padrão (stdout)
-        print("Saída:", resultado.stdout)
 
 # Chamar a função para executar o comando
 run_command()
+
