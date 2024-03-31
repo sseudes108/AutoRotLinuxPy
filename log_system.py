ef832c4ed3b6e857ab5188ace81b64efaff2807e
Basilísco <senstur.art@gmail.com>
2024-03-31 00:50:38 -0300

    Criar Log_Sytem.py

---
+++ log_system.py
@@ -0,0 +1,16 @@
+import subprocess
+import datetime
+
+from auto_rot_lib import catch_error
+
+
+def gerar_log():
+    processo = ["cat", "auth.log", "|", "grep", "$HOSTNAME", "|", "awk", "-F", "$HOSTNAME", "'{print $HOSTNAME $2}'",
+                "|", "grep", "ssh"]
+    data = datetime.datetime.now().strftime('%Y-%m-%d')
+    with open("w", encoding='utf-8') as new_log_file:
+        resultado = processo.run(data, processo, stdout=new_log_file, stderr=subprocess.PIPE)
+        catch_error(resultado, "log_rotina.py")
+
+
+gerar_log()
