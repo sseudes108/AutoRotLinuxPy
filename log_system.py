9cdf91050e2ed695d9cd6ee3184c38ecadd43bd7
Basilísco <senstur.art@gmail.com>
2024-03-31 00:52:51 -0300

    Atualizar Log_Sytem.py 2

--- log_system.py
+++ log_system.py
@@ -8,7 +8,7 @@ def gerar_log():
     processo = ["cat", "auth.log", "|", "grep", "$HOSTNAME", "|", "awk", "-F", "$HOSTNAME", "'{print $HOSTNAME $2}'",
                 "|", "grep", "ssh"]
     data = datetime.datetime.now().strftime('%Y-%m-%d')
-    with open("w", encoding='utf-8') as new_log_file:
+    with open("x", encoding='utf-8') as new_log_file:
         resultado = processo.run(data, processo, stdout=new_log_file, stderr=subprocess.PIPE)
         catch_error(resultado, "log_rotina.py")
 
