886b9c15402060a1dfe6dd8453dd445c75ef12b1
Basilísco <senstur.art@gmail.com>
2024-03-31 01:08:47 -0300

    Atualizar Log_Sytem.py 5

--- log_system.py
+++ log_system.py
@@ -5,12 +5,12 @@ from auto_rot_lib import catch_error
 
 
 def gerar_log():
-    processo = ["cat", "auth.log", "|", "grep", "$HOSTNAME", "|", "awk", "-F", "$HOSTNAME", "'{print $HOSTNAME $2}'",
-                "|", "grep", "ssh"]
+    processo = ["tail", "-n", "10", "/var/log/auth.log", "grep", "|", "ssh"]
     data = datetime.datetime.now().strftime('%Y-%m-%d')
     with open("new_log.txt", "w", encoding='utf-8') as new_log_file:
         new_log_file.write(data)
         resultado = subprocess.run(processo, stdout=new_log_file, stderr=subprocess.PIPE)
         catch_error(resultado, __name__)
+
 
 gerar_log()
