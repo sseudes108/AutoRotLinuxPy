e7749660ee5ecbbc3d2bb8009b72a8b75d654b74
Basilísco <senstur.art@gmail.com>
2024-03-31 01:23:50 -0300

    Atualizar Log_Sytem.py 10

--- log_system.py
+++ log_system.py
@@ -10,7 +10,7 @@ def gerar_log():
     data = datetime.datetime.now().strftime('%Y-%m-%d')
 
     with open("new_log.txt", "w", encoding='utf-8') as new_log_file:
-        new_log_file.write(data + '\n')
+        new_log_file.write(data + ':')
         resultado = subprocess.run(processo, stdout=subprocess.PIPE)
         resultado = subprocess.run(filtro, input=resultado.stdout, stdout=new_log_file, stderr=subprocess.PIPE, text=True)
         catch_error(resultado, "log_system.py")
