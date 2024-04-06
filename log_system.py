00d2f90bc8d161f9eae1a480d1ebb8f8b79637aa
Basilísco <senstur.art@gmail.com>
2024-04-06 10:02:03 -0300

    log rotina

--- log_system.py
+++ log_system.py
@@ -1,7 +1,7 @@
 import subprocess
 import datetime
 
-from auto_rot_lib import catch_error
+from auto_rot_lib import catch_error, get_log_path
 
 
 def ssh_server():
@@ -9,8 +9,9 @@ def ssh_server():
     filtro = ["grep", "-i", "ssh"]
     refino = ["cut", "-d", " ", "-f", "4-"]
     data = datetime.datetime.now().strftime('%Y-%m-%d')
+    log_path = get_log_path()
 
-    with open("auth_log_{}.txt".format(data), "w", encoding='utf-8') as new_log_file:
+    with open("{}auth_log_{}.txt".format(log_path, data), "w", encoding='utf-8') as new_log_file:
         resultado = subprocess.run(processo, stdout=subprocess.PIPE)
         resultado = subprocess.run(filtro, input=resultado.stdout, stdout=subprocess.PIPE)
         resultado = subprocess.run(refino, input=resultado.stdout, stdout=new_log_file, stderr=subprocess.PIPE)
