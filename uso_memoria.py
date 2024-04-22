55718c5bae5a6796b062e80da84f647e43ba8eef
Basilísco <senstur.art@gmail.com>
2024-04-22 17:48:34 -0300

    segunda iteração uso_memoria

--- uso_memoria.py
+++ uso_memoria.py
@@ -8,7 +8,7 @@ def uso_memoria():
     log_path = get_log_path()
 
     processo = ["ps", "-e", "-o", "pid", "|", "head", "-n", "11", "|", "grep", "[0-9]"]
-    with open("uso_memoria.txt.txt", "w", encoding='utf-8') as log_file:
+    with open("{}uso_memoria.txt".format(log_path), "w", encoding='utf-8') as log_file:
         resultado = subprocess.run(processo, stdout=log_file, stderr=subprocess.PIPE)
         catch_error(resultado, "uso_memoria.py")
-
+        log_file.write(resultado)
